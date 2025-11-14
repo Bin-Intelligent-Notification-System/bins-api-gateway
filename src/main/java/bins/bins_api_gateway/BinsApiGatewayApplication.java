fd1c434e83e8d159f4dde8146e7e63eefb43dd36
Gianluca Girardi <gl.girardi1@gmail.com>
2025-11-14 12:22:24 -0500

    first commit

---
+++ src/main/java/bins/bins_api_gateway/BinsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package bins.bins_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinsApiGatewayApplication.class, args);
+	}
+
+}
